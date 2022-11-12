0bc40346d9dd9d77df9ce97a9fb20318f02c2c94
Ari Jay <ikuskid7@gmail.com>
2022-11-12 22:47:23 +0100

    extract related methods & functionality to traits

--- src/Billable.php
+++ src/Billable.php
@@ -6,11 +6,16 @@ use Exception;
 use Carbon\Carbon;
 use InvalidArgumentException;
 use Illuminate\Support\Collection;
+use Jojostx\Cashier\Paystack\Concerns\ManagesCustomer;
+use Jojostx\Cashier\Paystack\Concerns\ManagesSubscriptions;
 use Unicodeveloper\Paystack\Facades\Paystack;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 trait Billable
 {
+    use ManagesCustomer;
+    use ManagesSubscriptions;
+
     /**
      * Make a "one off" or "recurring" charge on the customer for the given amount or plan respectively
      *
@@ -100,91 +105,6 @@ trait Billable
         return $this->tab($description, $amount, $options);
     }
 
-    /**
-     * Begin creating a new subscription.
-     *
-     * @param  string  $subscription
-     * @param  string  $plan
-     * @return \Jojostx\Cashier\Paystack\SubscriptionBuilder
-     */
-    public function newSubscription($subscription = 'default', $plan): SubscriptionBuilder
-    {
-        return new SubscriptionBuilder($this, $subscription, $plan);
-    }
-
-    /**
-     * Determine if the model is on trial.
-     *
-     * @param  string  $subscription
-     * @param  string|null  $plan
-     * @return bool
-     */
-    public function onTrial($subscription = 'default', $plan = null)
-    {
-        if (func_num_args() === 0 && $this->onGenericTrial()) {
-            return true;
-        }
-        $subscription = $this->subscription($subscription);
-        if (is_null($plan)) {
-            return $subscription && $subscription->onTrial();
-        }
-        return $subscription && $subscription->onTrial() &&
-            $subscription->paystack_plan === $plan;
-    }
-
-    /**
-     * Determine if the model is on a "generic" trial at the user level.
-     *
-     * @return bool
-     */
-    public function onGenericTrial()
-    {
-        return $this->trial_ends_at && Carbon::now()->lt($this->trial_ends_at);
-    }
-
-    /**
-     * Determine if the model has a given subscription.
-     *
-     * @param  string  $subscription
-     * @param  string|null  $plan
-     * @return bool
-     */
-    public function subscribed($subscription = 'default', $plan = null)
-    {
-        $subscription = $this->subscription($subscription);
-        if (is_null($subscription)) {
-            return false;
-        }
-        if (is_null($plan)) {
-            return $subscription->valid();
-        }
-        return $subscription->valid() &&
-            $subscription->paystack_plan === $plan;
-    }
-
-    /**
-     * Get a subscription instance by name.
-     *
-     * @param  string  $subscription
-     * @return \Jojostx\Cashier\Paystack\Subscription|null
-     */
-    public function subscription($subscription = 'default')
-    {
-        return $this->subscriptions->sortByDesc(function ($value) {
-            return $value->created_at->getTimestamp();
-        })->first(function ($value) use ($subscription) {
-            return $value->name === $subscription;
-        });
-    }
-    /**
-     * Get all of the subscriptions for the model.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function subscriptions()
-    {
-        return $this->hasMany(Subscription::class)->orderBy('created_at', 'desc');
-    }
     /**
      * Find an invoice by ID.
      *
@@ -308,85 +228,7 @@ trait Billable
             $card->delete();
         });
     }
-    /**
-     * Determine if the model is actively subscribed to one of the given plans.
-     *
-     * @param  array|string  $plans
-     * @param  string  $subscription
-     * @return bool
-     */
-    public function subscribedToPlan($plans, $subscription = 'default')
-    {
-        $subscription = $this->subscription($subscription);
-        if (!$subscription || !$subscription->valid()) {
-            return false;
-        }
-        foreach ((array) $plans as $plan) {
-            if ($subscription->paystack_plan === $plan) {
-                return true;
-            }
-        }
-        return false;
-    }
-    /**
-     * Determine if the entity is on the given plan.
-     *
-     * @param  string  $plan
-     * @return bool
-     */
-    public function onPlan($plan)
-    {
-        return !is_null($this->subscriptions->first(function ($subscription) use ($plan) {
-            return $subscription->paystack_plan === $plan;
-        }));
-    }
-
-    /**
-     * Create a Paystack customer for the given model.
-     *
-     * @param  string  $token
-     * @param  array  $options
-     * @throws \Exception
-     */
-    public function createAsPaystackCustomer(array $options = [])
-    {
-        $options = array_key_exists('email', $options)
-            ? $options
-            : array_merge($options, ['email' => $this->email]);
-
-        $response = PaystackService::createCustomer($options);
-
-        if (!$response['status']) {
-            throw new Exception('Unable to create Paystack customer: ' . $response['message']);
-        }
-        $this->paystack_id = $response['data']['id'];
-        $this->paystack_code = $response['data']['customer_code'];
-        $this->save();
-
-        return $response['data'];
-    }
-
-    /**
-     * Get the Paystack customer for the model.
-     *
-     * @return $customer
-     */
-    public function asPaystackCustomer()
-    {
-        $customer = Paystack::fetchCustomer($this->paystack_id)['data'];
-        return $customer;
-    }
-
-    /**
-     * Determine if the entity has a Paystack customer ID.
-     *
-     * @return bool
-     */
-    public function hasPaystackId()
-    {
-        return !is_null($this->paystack_id);
-    }
-
+  
     /**
      * Get the Paystack supported currency used by the entity.
      *
