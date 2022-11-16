5487877a13102c34b8032e01dc828e2f0f6a7a82
Ari Jay <ikuskid7@gmail.com>
2022-11-16 12:27:01 +0100

    update migration definitions

--- database/migrations/2022_05_03_000001_create_paystack_customers_table.php
+++ database/migrations/2022_05_03_000001_create_paystack_customers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
    */
   public function up()
   {
-    Schema::table('paystack_customers', function (Blueprint $table) {
+    Schema::create('paystack_customers', function (Blueprint $table) {
       $table->id();
       $table->unsignedBigInteger('billable_id');
       $table->string('billable_type');
