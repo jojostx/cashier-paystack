80e34d839710508ae23570b28c751d33e548aa6f
Ari Jay <ikuskid7@gmail.com>
2022-11-17 07:12:03 +0100

    add timestamps to migration

--- database/migrations/2022_05_03_000001_create_paystack_customers_table.php
+++ database/migrations/2022_05_03_000001_create_paystack_customers_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
       $table->string('card_brand')->nullable();
       $table->string('card_last_four', 4)->nullable();
       $table->timestamp('trial_ends_at')->nullable();
+      $table->timestamps();
 
       $table->index(['billable_id', 'billable_type']);
     });
